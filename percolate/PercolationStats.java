97cba64230855f52f46e9ef1dbf9f30b60015554
joeryan <joe@clanryan.us>
2014-07-23 20:53:12 -0400

    create PercolationStats class with javadoc

---
+++ percolate/PercolationStats.java
@@ -0,0 +1,56 @@
+/** PercolationStats.java - provides percolation statistics of a system
+  * defined by command line parameters. 
+  * Writen by Joe Ryan, 23 July, 2014.
+  * Usage: java PercolationStats [N] [T]
+  * Where [N] is the size of the test system grid (N-by-N) and
+  * where [T] indicates the number of times to run the Monte Carlo simulation
+  * outputs the mean of the percolation threshold, sstandard deviation, and 
+  * the 95% confidence interval as an upper and lower range
+  */
+
+public class PercolationStats throws IllegalArgumentException {
+    /** PercolationStats N-size experiments -> mean, stddev, 95% to stdout
+      * throws IllegalArgumentException if either N <= 0 or T <= 0
+      */
+    public PercolationStats(int N, int T) throws IllegalArgumentException {
+        /** perform T independent coputational experiments on an N-by-N grid
+          * Throws IllegalArgumentException if either N <= 0 or T <= 0
+          * @param N size of grid to perform experiments on 
+          * @param T number of independent simulations to perform
+          */
+        // TODO
+    }
+    
+    public double mean() {
+        /** sample mean of percolation threshold
+          */
+        // TODO
+    }
+    
+    public double stddev() {
+        /** sample standard deviation of percolation threshold
+          */
+        // TODO
+    }
+    
+    public double confidenceLo() {
+        /** returns lower bound of the 95% confidence interval
+          */
+        // TODO
+    }
+    
+    public double confidenceHi() {
+        /** returns upper bound of the 95% confidence interval
+          */
+        // TODO
+    }
+    
+    public static void main(String[] args) {
+        /** client takes two args N and T, perform T simulations on N-by-N grid
+          * @param args[] String command line argument to provide two parameters
+          * N and T to provide size of grid and number of simulations to run
+          */
+        // TODO
+        System.out.println("Test complete!");
+    }
+        
