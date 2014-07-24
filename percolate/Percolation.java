7124e9e73990a07bb03615d58a79c46f4f3289ed
joeryan <joe@clanryan.us>
2014-07-23 20:17:19 -0400

    initial Percolation.java commit, (prototypes)

---
+++ percolate/Percolation.java
@@ -0,0 +1,19 @@
+public class Percolation {
+    public Percolation(int N) {  // create N by N grid with all sites blocked
+        // TODO
+    }
+    public void open(int i, int j) {  // open site (row i, column j) if it not already open
+        // TODO
+    }
+    public boolean isOpen(int i, int j) {  // is site (row i, column j) open?
+        // TODO
+    }
+    public boolean isFull(int i, int j) {  // is site (row i, column j) full?
+        // TODO
+    }
+    public boolean percolates() {  // does the system percolate?
+        // TODO
+    }
+    
+    
+                         
