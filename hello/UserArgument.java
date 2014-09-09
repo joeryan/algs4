b22cda00c81f07bd14d27fbd562c0f3e79cd916d
joeryan <joe@clanryan.us>
2014-09-09 19:35:44 -0400

    inital work on learning java for algorithms UserArguments

---
+++ hello/UserArgument.java
@@ -0,0 +1,6 @@
+public class UserArgument {
+    public static void main(String[] args) {
+        String userName = new String(args[0]);
+        System.out.println("Hello, " + userName + "! How are you?");
+    }
+}
