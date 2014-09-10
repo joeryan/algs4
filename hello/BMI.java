5d5bbae7ca00e2741b5c040b73280e9187091ed0
joeryan <joe@clanryan.us>
2014-09-09 21:25:20 -0400

    update BMI.java for argument number checking

--- hello/BMI.java
+++ hello/BMI.java
@@ -7,10 +7,14 @@
 
 public class BMI {
     public static void main (String [] args) {
-        double weight = Double.parseDouble(args[0]);
-        double height = Double.parseDouble(args[1]);
-        double bmi = weight / (height * height);
-        System.out.println("BMI is: " + bmi);
+        if (args.length < 2) {
+            System.out.println("This program requires two arguments: weight and height.");
+        } else {
+            double weight = Double.parseDouble(args[0]);
+            double height = Double.parseDouble(args[1]);
+            double bmi = weight / (height * height);
+            System.out.println("BMI is: " + bmi);
+        }
     }
 }
 
